3c3de1d91a54a7ec8a52ed2f21b793b053c588e5
BattlemasterLoL <85181605+BattlemasterLoL@users.noreply.github.com>
2024-01-22 18:08:47 -0700

    v1.2.0 Update

--- CompartmentPk.py
+++ CompartmentPk.py
@@ -1,9 +1,11 @@
 import numpy as np
 from scipy.integrate import odeint
+import sympy as sym
 import matplotlib.pyplot as plt
 import tkinter as tk
 from tkinter import ttk, messagebox
 
+# Compile macOS: pyinstaller --collect-all matplotlib --onefile --windowed CompartmentPk.py
 
 def is_square(mat):
     return mat.shape[0] == mat.shape[1]
@@ -22,7 +24,79 @@ def get_user_input():
     return m, y0
 
 
+def find_eigenvalues_and_eigenvectors(matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    return eigenvalues, eigenvectors
+
+
+def find_coeff(eigenvectors, initial_values):
+    if len(initial_values) == 2:
+        # Equations below are determined by the compartment placement
+        a1, a2 = sym.symbols("a1, a2")
+        eq1 = sym.Eq(
+            (a1 * eigenvectors[0][1] + a2 * eigenvectors[0][0]), initial_values[0])
+        eq2 = sym.Eq(
+            (a1 * eigenvectors[1][1] + a2 * eigenvectors[1][0]), initial_values[1])
+        result = sym.solve([eq1, eq2], (a1, a2))
+        result = [result[a1], result[a2]]
+    elif len(initial_values) == 3:
+        # Equations below are determined by the compartment placement
+        a1, a2, a3 = sym.symbols("a1, a2, a3")
+        eq1 = sym.Eq((a1 * eigenvectors[0][2] + a2 * eigenvectors[0]
+                     [1] + a3 * eigenvectors[0][0]), initial_values[0])
+        eq2 = sym.Eq((a1 * eigenvectors[1][2] + a2 * eigenvectors[1]
+                     [1] + a3 * eigenvectors[1][0]), initial_values[1])
+        eq3 = sym.Eq((a1 * eigenvectors[2][2] + a2 * eigenvectors[2]
+                     [1] + a3 * eigenvectors[2][0]), initial_values[2])
+        result = sym.solve([eq1, eq2, eq3], (a1, a2, a3))
+        result = [result[a1], result[a2], result[a3]]
+    elif len(initial_values) == 4:
+        # Equations below are determined by the compartment placement
+        a1, a2, a3, a4 = sym.symbols("a1, a2, a3, a4")
+        eq1 = sym.Eq((a1 * eigenvectors[0][3] + a2 * eigenvectors[0][2] + a3 *
+                     eigenvectors[0][1] + a4 * eigenvectors[0][0]), initial_values[0])
+        eq2 = sym.Eq((a1 * eigenvectors[1][3] + a2 * eigenvectors[1][2] + a3 *
+                     eigenvectors[1][1] + a4 * eigenvectors[1][0]), initial_values[1])
+        eq3 = sym.Eq((a1 * eigenvectors[2][3] + a2 * eigenvectors[2][2] + a3 *
+                     eigenvectors[2][1] + a4 * eigenvectors[2][0]), initial_values[2])
+        eq4 = sym.Eq((a1 * eigenvectors[3][3] + a2 * eigenvectors[3][2] + a3 *
+                     eigenvectors[3][1] + a4 * eigenvectors[3][0]), initial_values[3])
+        result = sym.solve([eq1, eq2, eq3, eq4], (a1, a2, a3, a4))
+        result = [result[a1], result[a2], result[a3], result[a4]]
+    return result
+
+
+def const_eigen_window():
+    m, y0 = get_user_input()
+    eigvalues, eigvectors = find_eigenvalues_and_eigenvectors(m)
+    a_coeff = find_coeff(eigvectors, y0)
+    prefix = "a{}: "
+
+    window2 = tk.Toplevel()
+    window2.title('Extra Data')
+    window2.geometry('+700+75')
+    
+
+    # Creating a frame for each label
+    evecf = ttk.LabelFrame(window2, padding="10", text="Eigenvector")
+    evalf = ttk.LabelFrame(window2, padding="10", text="Eigenvalue")
+    cvalf = ttk.LabelFrame(window2, padding="10", text="a Coefficents")
+
+    # Gridding the frames
+    evecf.grid(row=0, column=0, padx=5, pady=5)
+    evalf.grid(row=1, column=0, padx=5, pady=5)
+    cvalf.grid(row=2, column=0, padx=5, pady=5)
+
+    # Packing the labels within each frame
+    ttk.Label(evecf, text=eigvectors).pack(padx=5, pady=5)
+    ttk.Label(evalf, text=eigvalues).pack(padx=5, pady=5)
+    for i, coeff in enumerate(a_coeff, start=1):
+        ttk.Label(cvalf, text=prefix.format(
+            i) + str(coeff)).pack(padx=5, pady=5)
+
+
 def run_simulation():
+    const_eigen_window()
     try:
         m, y0 = get_user_input()
         if is_square(m) and is_square(m.T):
@@ -33,34 +107,59 @@ def run_simulation():
             return
 
         def model(y, t):
-            dydt = -np.dot(m, y)  # ODE
+            dydt = -np.dot(m, y)          # ODE
             return dydt
 
-        t = np.linspace(0, 20)
-        y = odeint(model, y0, t)
+        def model_cont(y, t):
+            dydt = -np.dot(m, y) + y0     # ODE
+            return dydt
 
-        for i in range(len(m)):
-            plt.plot(t, y[:, i], label="Compartment " + str(i + 1))
-        print(type(y))
+        t = np.linspace(float(time_0.get()), float(time_f.get()), 100)
+        if cont_value.get() == 0:
+            y = odeint(model, y0, t)
+        else:
+            # for continuous injection
+            yin = np.zeros(len(y0))
+            y = odeint(model_cont, yin, t)
+
+        if time_skip.get() == '':
+            for i in range(len(m)):
+                plt.plot(t, y[:, i], label="Compartment " + str(i + 1))
+        else:
+            y_new = y.copy()
+            tskip = float(time_skip.get())
+            time_index = int(np.where(t >= tskip)[0][0])
+            idx = time_index
+            # for repeated injection
+            while idx < len(t):
+                ypad = np.pad(y, ((int(idx), 0), (0, 0)),
+                              mode='constant', constant_values=0)
+                ypad = ypad[0:len(y), :]
+                y_new = y_new + ypad
+                idx = idx + time_index
+            for i in range(len(m)):
+                plt.plot(t, y_new[:, i], label="Compartment " + str(i + 1))
 
         plt.xlabel("Time (hrs)")
         plt.ylabel("Concentration (nM)")
         plt.legend()
         plt.show()
 
+
     except Exception as e:
         messagebox.showerror("Error", str(e))
 
 
 root = tk.Tk()
-root.title("ODE Solver")
+root.title("Compartment Pk Solver")
 
 frame_main = ttk.Frame(root, padding="10")
 frame_main.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
 
 frame_compartments = ttk.LabelFrame(
     frame_main, padding="10", text="Compartments")
-frame_compartments.grid(row=0, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+frame_compartments.grid(row=0, column=0, columnspan=2,
+                        sticky=(tk.W, tk.E, tk.N, tk.S))
 
 input_compartments = ttk.Entry(frame_compartments, width=10)
 input_compartments.grid(row=0, column=0, padx=5, pady=5, sticky=tk.W)
@@ -69,7 +168,8 @@ ttk.Label(frame_compartments, text="Enter the number of compartments: ").grid(
 )
 
 frame_m_matrix = ttk.LabelFrame(frame_main, padding="10", text="m Matrix")
-frame_m_matrix.grid(row=1, column=0, sticky=(tk.W, tk.E, tk.N, tk.S))
+frame_m_matrix.grid(row=1, column=0, columnspan=2,
+                    sticky=(tk.W, tk.E, tk.N, tk.S))
 
 input_m_matrix = []
 for i in range(4):
@@ -90,8 +190,29 @@ for i in range(4):
     entry.grid(row=i, column=0, padx=5, pady=5, sticky=tk.W)
     input_y0_vector.append(entry)
 
+frame_t_values = ttk.LabelFrame(frame_main, padding="10", text="Time Values")
+frame_t_values.grid(row=2, column=1, sticky=(tk.W, tk.E, tk.N, tk.S))
+
+ttk.Label(frame_t_values, text='Initial: ').grid(
+    row=0, column=0, padx=5, pady=5)
+ttk.Label(frame_t_values, text='Final: ').grid(row=1, column=0, padx=5, pady=5)
+ttk.Label(frame_t_values, text='Skip: ').grid(row=2, column=0, padx=5, pady=5)
+
+time_0 = ttk.Entry(frame_t_values, width=10)
+time_0.grid(row=0, column=1, padx=5, pady=5, sticky=tk.W)
+time_f = ttk.Entry(frame_t_values, width=10)
+time_f.grid(row=1, column=1, padx=5, pady=5, sticky=tk.W)
+time_skip = ttk.Entry(frame_t_values, width=10)
+time_skip.grid(row=2, column=1, padx=5, pady=5, sticky=tk.W)
+
 ttk.Button(frame_main, text="Run Simulation", command=run_simulation).grid(
-    row=3, column=0, columnspan=4, padx=5, pady=5
+    row=3, column=0, padx=5, pady=5
 )
+
+cont_value = tk.IntVar(value=0)
+continuous_check = ttk.Checkbutton(frame_main, text="Continuous Input", onvalue=1, offvalue=0, variable=cont_value).grid(
+    row=3, column=1, padx=5, pady=5
+)
+
 
 root.mainloop()
--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Compartment ODE Solver
 
-Compartment ODE solver is a tool focused in solving molecule transfer between compartments problems that are part of the University of Utah BME 4001 - bioTransport class. In using this tool known data can be entered to create a concentration vs time plot. This program is designed to solve compartment ODE's in the form:
+Compartment ODE solver is a tool focused in solving molecule transfer between compartments problems that are part of the University of Utah BME 4001 - bioTransport class. In using this tool known data can be entered to create a concentration vs time plot and solve for additional data values. This program is designed to solve compartment ODE's in the form:
 
-$$ \frac{d\vec{C}}{dt} + \overline{m}\,\vec{C} = \vec{F} $$
+$$ \frac{d\vec{C}}{dt} + \overline{m}\,\vec{C} = \vec{F} \tag {1}$$
 
 and will output a concentration vs time plot. The m matrix ($\,\overline{m}\,$) is a ratio of the concentration coefficents ($\,k_{ij}\,$) and compartment volumes ($\,V\,$) in the form $n \times n$ matrix in the form:
 
@@ -14,13 +14,19 @@ m_{21} & m_{22} & m_{23} & m_{24}\\
 m_{31} & m_{32} & m_{33} & m_{34}\\
 m_{41} & m_{42} & m_{43} & m_{44}\\
 \end{bmatrix}
+\tag {2}
 $$
+
+The constants $a_n$ and $\alpha_n$ values in the equation:
+$$ C(t) = a_1 e^{-\alpha_1 t} + a_2 e^{-\alpha_2 t} + ... + a_n e^{\alpha_n t} \tag {3}$$
+Eigenvalues and eigenvectors additionally solved and are populated in the extra data window with the $a_n$ and $\alpha_n$ values.
 
 Required Modules:
 <br/>
 
 [![numpy](https://img.shields.io/badge/numpy-1.26.0-blue)](https://numpy.org/)
 [![scipy](https://img.shields.io/badge/scipy-1.11.4-blue)](https://scipy.org/)
+[![sympy](https://img.shields.io/badge/sympy-1.11.1-blue)](https://sympy.org)
 [![matplotlib](https://img.shields.io/badge/matplotlib-3.8-blue)](https://matplotlib.org/)
 [![tkinter](https://img.shields.io/badge/tkinter-3.12.1-blue)](https://docs.python.org/3/library/tkinter.html)
 
@@ -38,11 +44,15 @@ Terminal must be active in the same directory as the python program to launch.
 
 ### Windows
 
-For windows users a compiled app is availible [here](comp_ODE_TK.exe). The program may also be launched using ther terminal command above.
+For windows users a compiled app is availible [here](CompartmentPk.exe). The program may also be launched using ther terminal command above.
 
-### MacOS/Linux
+### Windows
 
-For macOS and linux users a compiled app is not avalible and muse be lunched using there terminal outlined above.
+For macOS users a compiled app is availible [here](CompartmentPk.app). The program may also be launched using ther terminal command above.
+
+### Linux
+
+For linux users a compiled app is not avalible and muse be lunched using there terminal outlined above.
 
 ## Documentation
 
@@ -58,17 +68,22 @@ This section of the program consists of a $4 \times 4$ grid of boxes to enter nu
 
 ### Initial Conditions
 
-This section of the program consists of 4 boxes to enter the values of the known initial conditions of each of the compartments. These values must be numarical and may contain desimal values. The order of the initial condition boxes are with the top being the first compartment and assinding in order as you move down. If the number of compartments is less than 4 you do not need to enter values into the extra boxes.
+This section of the program consists of 4 boxes to enter the values of the known initial conditions of each of the compartments. These values must be numarical and may contain desimal values. The order of the initial condition boxes are with the top being the first compartment and assinding in order as you move down. If the number of compartments is less than 4 you do not need to enter values into the extra boxes. If the continuous input box is checked the initial conditions of each compartment is set to 0 and these inputs will be for the input over time for each compartment.
 
 ### Time Settings
+
 This secion of the program consists of 3 boxes to enter the start time, end time, and time between input (skip) of molecules into compartments. The start time or initial is the time value where the plot time will begin. The end time or final is where the plot time will end. If multiple inputs of molecules is not occuring leave the skip box blank.
+
+### Continuous Input
+
+Next to the run simulation button is a checkbox (default off) for continuous input. When this box is pressed the program will simulate a continuous input over time. This changes the initial conditions section input to be the continuous input over time conditions for each compartment. This causes the initial conditions to be set to 0 for all compartments.
 
 ### Run Simulation
 
-This button is to be pressed after all values in the previous sections are filled out. By pressing this putton the ODE will be solved for time values from 0-20 and a plot will be created in a new window. This plot may then be saved by pressing the save icon in the plot window.
+This button is to be pressed after all values in the previous sections are filled out. By pressing this putton the ODE will be solved for time values set in the time setting section and a plot will be created in a new window. This plot may then be saved by pressing the save icon in the plot window (see plot example below). An additional window will be generated containing the eigenvectors, eigenvalues and a coefficents for equation 2 and 3 as outlined above.
 
 ## Examples
 
-|               <h3>Main Program</h3>                |              <h3>Plot</h3>              |
-| :------------------------------------------------: | :-------------------------------------: |
-| ![Main Program Example Image](program_example.png) | ![Plot Example Image](plot_example.png) |
+|               <h3>Main Program</h3>                |              <h3>Plot</h3>              |        <h3>Eigen & Constants Data</h3>        |
+| :------------------------------------------------: | :-------------------------------------: | :-------------------------------------------: |
+| ![Main Program Example Image](program_example.png) | ![Plot Example Image](plot_example.png) | ![Eigen Constant Data](eig_coeff_example.png) |
